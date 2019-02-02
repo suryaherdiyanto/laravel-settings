e968dfeaf5a28f2bc4223ab1acb4369b7b7141df
surya <suryaherdiyanto@gmail.com>
2019-02-02 11:50:16 +0700

    init

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
