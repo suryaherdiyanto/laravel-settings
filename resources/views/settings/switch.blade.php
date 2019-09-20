e5607867e2cc096e63b9f6a45e84b7dd02654d6f
surya herdiyanto <suryaherdiyanto@gmail.com>
2019-09-20 22:09:36 +0800

    remove echo value on switch

--- resources/views/settings/switch.blade.php
+++ resources/views/settings/switch.blade.php
@@ -1,4 +1,3 @@
-{{ $value }}
 <div class="custom-control custom-switch">
     <input type="checkbox" class="custom-control-input" id="switch-{{ $i }}" value="1" name="{{ $name }}" {{ ((bool)$value === true) ? 'checked':'' }}>
     <label class="custom-control-label" for="switch-{{ $i }}">{{ $label }}</label>
